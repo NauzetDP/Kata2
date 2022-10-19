1d6fd7dcc1186755056c0b020172f6b80d469388
Nauzet <nauzetdenizperdomo@hotmail.es>
2022-10-19 18:22:01 +0100

    Commit Kata2 versión 5

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class Kata2 {
 
     public static void main(String[] args) {
-        Integer [] data = {9, 2, 3, 4, 5, 8, 1, 2, 3, 9, 5, 3, 6, 2, 7, 3, 6, 0};
+        String [] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
         
         Histogram histogram = new Histogram(data);
         
